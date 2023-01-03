93edf71ae9af453ec41728f3dce65056a2838945
Simon Biquard <sbiquard@gmail.com>
2023-01-03 05:49:48 -0800

    rewriting of noise weighting pcg * reset tod gaps at the beginning * print messages at each iteration if verbose * in all cases print nbr of iterations and solve time at the end * increase max number of iterations

--- mappraiser/src/pcg_true.c
+++ mappraiser/src/pcg_true.c
@@ -18,9 +18,11 @@
 #include <unistd.h>
 #include "mappraiser.h"
 
-int apply_weights(Tpltz *Nm1, Tpltz *Ncov, Gap *Gaps, double *tod, int m, int rank);
+int apply_weights(Tpltz *Nm1, Tpltz *N, Gap *Gaps, double *tod, int m, int rank, int verbose);
 
-int PCG_GLS_true(char *outpath, char *ref, Mat *A, Tpltz *Nm1, Tpltz *Ncov, double *x, double *b, double *noise, double *cond, int *lhits, double tol, int K, int precond, int Z_2lvl, Gap *Gaps, int64_t gif)
+int reset_tod_gaps(double *tod, Tpltz *N, Gap *Gaps);
+
+int PCG_GLS_true(char *outpath, char *ref, Mat *A, Tpltz *Nm1, Tpltz *N, double *x, double *b, double *noise, double *cond, int *lhits, double tol, int K, int precond, int Z_2lvl, Gap *Gaps, int64_t gif)
 {
     int i, j, k; // some indexes
     int m, n;    // number of local time samples, number of local pixels
@@ -58,9 +60,9 @@ int PCG_GLS_true(char *outpath, char *ref, Mat *A, Tpltz *Nm1, Tpltz *Ncov, doub
     t = MPI_Wtime();
     if (rank == 0)
     {
-        printf("[rank %d] Preconditioner computation time = %lf \n", rank, t - st);
-        // printf("[rank %d] trash_pix flag = %d \n", rank, A->trash_pix);
-        // printf("[rank %d] nbr sky pixels = %d \n", rank, n);
+        printf("[rank %d] Preconditioner computation time = %lf\n", rank, t - st);
+        printf("[rank %d] trash_pix flag = %d\n", rank, A->trash_pix);
+        printf("[rank %d] nbr sky pixels = %d\n", rank, n);
         fflush(stdout);
     }
 
@@ -85,7 +87,10 @@ int PCG_GLS_true(char *outpath, char *ref, Mat *A, Tpltz *Nm1, Tpltz *Ncov, doub
     for (i = 0; i < m; i++)
         _g[i] = b[i] + noise[i] - _g[i];
 
-    apply_weights(Nm1, Ncov, Gaps, _g, A->m, rank); // _g = Nm1 (d-Ax0)  (d = signal + noise)
+    apply_weights(Nm1, N, Gaps, _g, A->m, rank, 0); // _g = Nm1 (d-Ax0)  (d = signal + noise)
+
+    // debug: look at one execution of noise weighting PCG
+    // exit(0);
 
     TrMatVecProd(A, _g, g, 0); // g = At _g
 
@@ -155,7 +160,7 @@ int PCG_GLS_true(char *outpath, char *ref, Mat *A, Tpltz *Nm1, Tpltz *Ncov, doub
 
         MatVecProd(A, h, Ah, 0); // Ah = A h
 
-        apply_weights(Nm1, Ncov, Gaps, Nm1Ah, A->m, rank); // Nm1Ah = Nm1 Ah   (Nm1Ah == Ah)
+        apply_weights(Nm1, N, Gaps, Nm1Ah, A->m, rank, 0); // Nm1Ah = Nm1 Ah   (Nm1Ah == Ah)
 
         TrMatVecProd(A, Nm1Ah, AtNm1Ah, 0); // AtNm1Ah = At Nm1Ah
 
@@ -265,100 +270,118 @@ int PCG_GLS_true(char *outpath, char *ref, Mat *A, Tpltz *Nm1, Tpltz *Ncov, doub
 }
 
 /* Weights TOD data according to the adopted noise model*/
-int apply_weights(Tpltz *Nm1, Tpltz *Ncov, Gap *Gaps, double *tod, int m, int rank)
+int apply_weights(Tpltz *Nm1, Tpltz *N, Gap *Gaps, double *tod, int m, int rank, int verbose)
 {
     int t_id; // time sample index in local data
     int i, j;
 
-    if (Gaps->ngap == 0) /* No gaps in the timestream */
+    if (Nm1->tpltzblocks[0].lambda == 1) /* Use straightforward loop for white noise model */
     {
-        // Use straightforward loop for white noise model
-        if (Nm1->tpltzblocks[0].lambda == 1)
+        // Here it is assumed that we use a single bandwidth for all TOD intervals
+        // i.e. lambda is the same for all Toeplitz blocks
+        t_id = 0;
+        for (i = 0; i < Nm1->nb_blocks_loc; i++)
         {
-            // Here it is assumed that we use a single bandwidth for all TOD intervals, i.e. lambda is the same for all Toeplitz blocks
-            t_id = 0;
-            for (i = 0; i < Nm1->nb_blocks_loc; i++)
+            for (j = 0; j < Nm1->tpltzblocks[i].n; j++)
             {
-                for (j = 0; j < Nm1->tpltzblocks[i].n; j++)
-                {
-                    tod[t_id + j] = Nm1->tpltzblocks[i].T_block[0] * tod[t_id + j];
-                }
-                t_id += Nm1->tpltzblocks[i].n;
+                tod[t_id + j] = Nm1->tpltzblocks[i].T_block[0] * tod[t_id + j];
             }
+            t_id += Nm1->tpltzblocks[i].n;
         }
-        // Use stbmmProd routine for correlated noise model (No det-det correlations for now)
-        else
-            stbmmProd(Nm1, tod);
+    }
+    else if (Gaps->ngap == 0) /* No gaps in the timestream */
+    {
+        // Use stbmmProd routine for correlated noise model
+        // (no det-det correlations for now)
+        stbmmProd(Nm1, tod);
     }
     else /* Use PCG + gstbmmProd to apply the noise weights */
     {
-        const int kmax = 50;     // maximum iteration count
+        const int kmax = 100;    // maximum iteration count
         const double tol = 1e-6; // convergence criterion
         int k = 0;               // iteration number
         double r0, res, tol2rel; // residuals
         double coef_1, coef_2;   // scalars
         double lval_1, lval_2;   // local values before reduction
+        double st, t;            // timing variables
 
-        double *x, *p = NULL;      // guess, search direction
-        double *_r, *r, *z = NULL; // gradient
+        double *p = NULL, *_r = NULL, *r = NULL, *z = NULL; // time-domain objects
 
-        x = malloc((sizeof *x) * m);
-        p = malloc((sizeof *p) * m);
+        st = MPI_Wtime();
+
+        // the rhs should have gaps full of 0
+        reset_tod_gaps(tod, N, Gaps);
+
         _r = malloc((sizeof *_r) * m);
         r = malloc((sizeof *r) * m);
-        z = malloc((sizeof *z) * m);
 
-        if (x == NULL || p == NULL || _r == NULL || r == NULL || z == NULL)
+        if (_r == NULL || r == NULL)
         {
             puts("out of memory: allocation of time-domain vectors for tod weighting failed");
             exit(EXIT_FAILURE);
         }
 
-        // initial guess
-        for (i = 0; i < m; ++i)
-            _r[i] = tod[i];
-
-        // apply system matrix (_r = Nx0)
-        gstbmmProd(Ncov, _r, Gaps);
-
-        // compute initial residual (r = b - Nx0)
         for (i = 0; i < m; ++i)
         {
-            r[i] = tod[i] - _r[i];
-            z[i] = r[i];
+            _r[i] = 0.;    // set initial guess to zero
+            r[i] = tod[i]; // need rhs to compute first residual
+            tod[i] = _r[i];
         }
 
-        // apply preconditioner (z0 = M^{-1} * r0)
-        gstbmmProd(Nm1, z, Gaps);
+        // apply system matrix (_r = Nx0)
+        gstbmmProd(N, _r, Gaps);
 
-        // set initial search direction (p0 = z0)
-        for (i = 0; i < m; ++i)
-            p[i] = z[i];
-
-        // check convergence
+        // compute initial residual (r = b - Nx0)
+        // check for instant convergence
         lval_1 = 0.0;
-        for (i = 0; i < m; i++)
+        for (i = 0; i < m; ++i)
+        {
+            r[i] = r[i] - _r[i];
             lval_1 += r[i] * r[i];
+        }
 
         res = 0.0;
         MPI_Allreduce(&lval_1, &res, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
         r0 = res;
         tol2rel = tol * tol * res;
 
-        if (rank == 0) /* print information on screen */
+        t = MPI_Wtime();
+
+        if (rank == 0 && verbose) /* print information on screen */
         {
-            printf("[noise weighting PCG] k = %d, r0 = %e\n", k, r0);
+            printf("[noise weighting PCG] k = %d, r0 = %e, time=%lf\n", k, r0, t - st);
         }
 
         if (res < tol * tol)
         {
             // print info on screen
-            if (rank == 0)
+            if (rank == 0 && verbose)
                 printf("  -> converged (%e < %e)\n", res, tol * tol);
 
-            // do not enter the PCG loop
-            k = kmax;
+            // return immediately
+            free(_r);
+            free(r);
+            return 0;
         }
+
+        p = malloc((sizeof *p) * m);
+        z = malloc((sizeof *z) * m);
+        if (p == NULL || z == NULL)
+        {
+            puts("out of memory: allocation of time-domain vectors for tod weighting failed");
+            exit(EXIT_FAILURE);
+        }
+
+        t = MPI_Wtime();
+
+        // apply preconditioner (z0 = M^{-1} * r0)
+        for (i = 0; i < m; ++i)
+            z[i] = r[i];
+        gstbmmProd(Nm1, z, Gaps);
+
+        // set initial search direction (p0 = z0)
+        for (i = 0; i < m; ++i)
+            p[i] = z[i];
 
         for (k = 1; k < kmax; ++k)
         {
@@ -368,7 +391,7 @@ int apply_weights(Tpltz *Nm1, Tpltz *Ncov, Gap *Gaps, double *tod, int m, int ra
             for (i = 0; i < m; ++i)
                 _r[i] = p[i];
 
-            gstbmmProd(Ncov, _r, Gaps);
+            gstbmmProd(N, _r, Gaps);
 
             // compute alpha = (r,z)/(p,Np)
             lval_1 = 0.0;
@@ -388,7 +411,7 @@ int apply_weights(Tpltz *Nm1, Tpltz *Ncov, Gap *Gaps, double *tod, int m, int ra
             // update residual (r = r - alpha * Np)
             for (i = 0; i < m; ++i)
             {
-                x[i] = x[i] + (coef_1 / coef_2) * p[i];
+                tod[i] = tod[i] + (coef_1 / coef_2) * p[i];
                 r[i] = r[i] - (coef_1 / coef_2) * _r[i];
                 z[i] = r[i];
             }
@@ -415,24 +438,43 @@ int apply_weights(Tpltz *Nm1, Tpltz *Ncov, Gap *Gaps, double *tod, int m, int ra
             res = 0.0;
             MPI_Allreduce(&lval_1, &res, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
-            if (rank == 0)
+            if (rank == 0 && verbose)
             {
                 printf("[k = %d] res = %e, relative = %e\n", k, res, sqrt(res / r0));
             }
 
             if (res < tol2rel)
             {
+                t = MPI_Wtime();
                 if (rank == 0)
                 {
-                    printf("  -> converged (%e < %e) \n", res, tol2rel);
-                    printf("  -> i.e.      (%e < %e) \n", sqrt(res / r0), tol);
-                    // printf("  -> solve time = %lf \n", solve_time);
+                    if (verbose)
+                    {
+                        printf("  -> converged (%e < %e)\n", res, tol2rel);
+                        printf("  -> i.e.      (%e < %e)\n", sqrt(res / r0), tol);
+                        printf("  -> n_iter = %d, solve time = %lf\n", k, t - st);
+                    }
+                    else
+                    {
+                        printf("  -> applied noise weights in %d iterations (%lf seconds)\n", k, t - st);
+                    }
                 }
                 break;
             }
         }
         fflush(stdout);
+
+        // free memory
+        free(p);
+        free(_r);
+        free(r);
+        free(z);
     }
 
     return 0;
+}
+
+int reset_tod_gaps(double *tod, Tpltz *N, Gap *Gaps)
+{
+    reset_gaps(&tod, N->idp, N->local_V_size, N->m_cw, N->nrow, N->m_rw, Gaps->id0gap, Gaps->lgap, Gaps->ngap);
 }
