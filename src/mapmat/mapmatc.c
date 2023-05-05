62e8bbee369cbfe9157e91ef559e1d3dcb05eed2
Hamza El Bouhargani <elbouha@apc.in2p3.fr>
2023-05-05 10:40:41 -0700

    Various fixes to doc

--- src/mapmat/mapmatc.c
+++ src/mapmat/mapmatc.c
@@ -1,4 +1,4 @@
-/** @file:wq   mapmatc.c
+/** @file:  mapmatc.c
     @brief  Sparse Matrix with Coarse Level 
     @note  Copyright (c) 2010-2012 APC CNRS Université Paris Diderot. This program is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. You should have received a copy of the GNU General Public License along with this program; if not, see http://www.gnu.org/licenses/lgpl.html
 
