faba04fd3d3448e80bb8779699b849d12c23321c
Simon Biquard <sbiquard@gmail.com>
2023-10-04 14:16:20 +0200

    treat shared/non-shared pixels correctly in PCG scalar products computations

--- mappraiser/include/mappraiser/precond.h
+++ mappraiser/include/mappraiser/precond.h
@@ -4,12 +4,14 @@
 #include <midapack.h>
 
 typedef struct precond_t {
-    int precond; // 0 = BJ, 1 = 2lvl a priori, 2 = 2lvl a posteriori
-    int n;
-    int Zn;
-    Mat BJ_inv;
-    Mat BJ;
-    double *pixpond;
+    int precond;     // 0 = BJ, 1 = 2lvl a priori, 2 = 2lvl a posteriori
+    int n;           // total number of pixels (= n_valid + n_extra)
+    int n_valid;     // number of valid pixels
+    int n_extra;     // number of extra pixels
+    double *pixpond; // pixel share ponderation
+    Mat BJ_inv;      // inverse preconditioner matrix
+    Mat BJ;          // preconditioner matrix
+    int Zn;          // size of deflation space (2-lvl preconditioners)
 
     /* 2 lvl only (NULL otherwise) */
     double **Z;
--- mappraiser/src/pcg_true.c
+++ mappraiser/src/pcg_true.c
@@ -19,6 +19,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double scalar_prod_reduce(Mat *A, Precond *P, const double *p1,
+                          const double *p2, const double *p_sub);
+
 int PCG_GLS_true(char *outpath, char *ref, Mat *A, Tpltz *Nm1, Tpltz *N,
                  double *x, double *b, double *noise, double *cond, int *lhits,
                  double tol, int K, int precond, int Z_2lvl,
@@ -38,7 +41,7 @@ int PCG_GLS_true(char *outpath, char *ref, Mat *A, Tpltz *Nm1, Tpltz *N,
     double *g, *gp, *gt, *Cg, *h; // map domain vectors
     double *AtNm1Ah;              // map domain
     double ro, gamma, coeff;      // scalars
-    double g2pix, g2pixp, g2pix_polak;
+    double g2pix, g2pix_prev, g2pix_polak;
 
     // if we want to use the true norm to compute the residual
     int TRUE_NORM = 1; // 0: No ; 1: Yes
@@ -60,8 +63,8 @@ int PCG_GLS_true(char *outpath, char *ref, Mat *A, Tpltz *Nm1, Tpltz *N,
     if (Z_2lvl == 0)
         Z_2lvl = size;
 
-    build_precond(&p, &pixpond, &n, A, Nm1, &x, b, noise, cond, lhits, tol,
-                  Z_2lvl, precond, Gaps, gif);
+    build_precond(&p, &pixpond, A, Nm1, &x, b, noise, cond, lhits, tol, Z_2lvl,
+                  precond, Gaps, gif);
 
     MPI_Barrier(A->comm);
     t = MPI_Wtime();
@@ -69,8 +72,9 @@ int PCG_GLS_true(char *outpath, char *ref, Mat *A, Tpltz *Nm1, Tpltz *N,
     if (rank == 0) {
         printf("[rank %d] Preconditioner computation time = %lf\n", rank,
                t - st);
-        printf("  -> trash_pix flag = %d\n", A->trash_pix);
-        printf("  -> nbr sky pixels = %d\n", n);
+        printf("  -> nbr of sky pixels = %d\n", p->n);
+        printf("  -> valid pixels = %d\n", p->n_valid);
+        printf("  -> extra pixels = %d\n", p->n_extra);
         fflush(stdout);
     }
 
@@ -152,10 +156,10 @@ int PCG_GLS_true(char *outpath, char *ref, Mat *A, Tpltz *Nm1, Tpltz *N,
     }
 
     // map domain objects memory allocation
-    h = (double *)malloc(n * sizeof(double));  // descent direction
-    g = (double *)malloc(n * sizeof(double));  // residual
-    gp = (double *)malloc(n * sizeof(double)); // previous residual
-    AtNm1Ah = (double *)malloc(n * sizeof(double));
+    h = (double *)malloc(p->n * sizeof(double));  // descent direction
+    g = (double *)malloc(p->n * sizeof(double));  // residual
+    gp = (double *)malloc(p->n * sizeof(double)); // previous residual
+    AtNm1Ah = (double *)malloc(p->n * sizeof(double));
 
     // time domain objects memory allocation
     Ah = (double *)malloc(m * sizeof(double));
@@ -179,27 +183,18 @@ int PCG_GLS_true(char *outpath, char *ref, Mat *A, Tpltz *Nm1, Tpltz *N,
 
     apply_precond(p, A, Nm1, g, Cg);
 
-    for (j = 0; j < n; j++) // h = Cg
+    for (j = 0; j < p->n; j++) // h = Cg
         h[j] = Cg[j];
 
-    g2pix = 0.0; // g2pix = "Cg res"
-    localreduce = 0.0;
-    for (i = 0; i < n; i++) // g2pix = (Cg, g)
-        localreduce += Cg[i] * g[i] * pixpond[i];
-
-    MPI_Allreduce(&localreduce, &g2pix, 1, MPI_DOUBLE, MPI_SUM, A->comm);
+    // g2pix = (Cg, g)
+    g2pix = scalar_prod_reduce(A, p, Cg, g, NULL);
 
     t = MPI_Wtime();
     solve_time += (t - st);
 
     if (TRUE_NORM == 1) {
-        res = 0.0; // g2 = "res"
-        localreduce = 0.0;
-        for (i = 0; i < n; i++) // g2 = (g, g)
-            localreduce += g[i] * g[i] * pixpond[i];
-
-        MPI_Allreduce(&localreduce, &res, 1, MPI_DOUBLE, MPI_SUM,
-                      MPI_COMM_WORLD);
+        // res = (g, g)
+        res = scalar_prod_reduce(A, p, g, g, NULL);
     } else {
         res = g2pix;
     }
@@ -247,48 +242,29 @@ int PCG_GLS_true(char *outpath, char *ref, Mat *A, Tpltz *Nm1, Tpltz *N,
 
         TrMatVecProd(A, Nm1Ah, AtNm1Ah, 0); // AtNm1Ah = At Nm1Ah
 
-        coeff = 0.0;
-        localreduce = 0.0;
-        for (i = 0; i < n; i++)
-            localreduce += h[i] * AtNm1Ah[i] * pixpond[i];
-        MPI_Allreduce(&localreduce, &coeff, 1, MPI_DOUBLE, MPI_SUM,
-                      MPI_COMM_WORLD);
+        coeff = scalar_prod_reduce(A, p, h, AtNm1Ah, NULL);
 
         ro = g2pix / coeff;
 
-        for (j = 0; j < n; j++) // x = x + ro * h
+        for (j = 0; j < p->n; j++) // x = x + ro * h
             x[j] = x[j] + ro * h[j];
 
-        for (j = 0; j < n; j++)             // g = g + ro * (At Nm1 A) h
+        for (j = 0; j < p->n; j++)          // g = g + ro * (At Nm1 A) h
             g[j] = gp[j] - ro * AtNm1Ah[j]; // Use Ribière-Polak formula
 
         apply_precond(p, A, Nm1, g, Cg);
 
-        g2pixp = g2pix; // g2p = "res"
-        localreduce = 0.0;
-        for (i = 0; i < n; i++) // g2 = (Cg, g)
-            localreduce += Cg[i] * g[i] * pixpond[i];
+        g2pix_prev = g2pix; // g2p = "res"
+        g2pix = scalar_prod_reduce(A, p, Cg, g, NULL);
 
-        MPI_Allreduce(&localreduce, &g2pix, 1, MPI_DOUBLE, MPI_SUM, A->comm);
-
-        localreduce = 0.0;
-        for (i = 0; i < n; i++) // g2 = (Cg, g)
-            localreduce += Cg[i] * (g[i] - gp[i]) * pixpond[i];
-
-        MPI_Allreduce(&localreduce, &g2pix_polak, 1, MPI_DOUBLE, MPI_SUM,
-                      A->comm);
+        g2pix_polak = scalar_prod_reduce(A, p, Cg, g, gp);
 
         t = MPI_Wtime();
         solve_time += (t - st);
 
         // Just to check with the true norm:
         if (TRUE_NORM == 1) {
-            localreduce = 0.0;
-            for (i = 0; i < n; i++) // g2 = (Cg, g)
-                localreduce += g[i] * g[i] * pixpond[i];
-
-            MPI_Allreduce(&localreduce, &res, 1, MPI_DOUBLE, MPI_SUM,
-                          MPI_COMM_WORLD);
+            res = scalar_prod_reduce(A, p, g, g, NULL);
         } else {
             res = g2pix_polak;
         }
@@ -313,17 +289,17 @@ int PCG_GLS_true(char *outpath, char *ref, Mat *A, Tpltz *Nm1, Tpltz *N,
             break;
         }
 
-        if (g2pix_polak > g2pixp) {
+        if (g2pix_polak > g2pix_prev) {
             if (rank == 0)
-                printf("--> g2pix > g2pixp pb (%e > %e) \n", g2pix, g2pixp);
+                printf("--> g2pix > g2pixp pb (%e > %e) \n", g2pix, g2pix_prev);
         }
 
         st = MPI_Wtime();
 
         // gamma = g2pix / g2pixp;
-        gamma = g2pix_polak / g2pixp;
+        gamma = g2pix_polak / g2pix_prev;
 
-        for (j = 0; j < n; j++) // h = h * gamma + Cg
+        for (j = 0; j < p->n; j++) // h = h * gamma + Cg
             h[j] = h[j] * gamma + Cg[j];
 
     } // End loop
@@ -347,4 +323,34 @@ int PCG_GLS_true(char *outpath, char *ref, Mat *A, Tpltz *Nm1, Tpltz *N,
     free_precond(&p);
 
     return 0;
+}
+
+double scalar_prod_reduce(Mat *A, Precond *P, const double *p1,
+                          const double *p2, const double *p_sub) {
+    double result;
+
+    // contributions from valid pixels
+    double local_sum = 0.0;
+    for (int i = P->n_extra; i < P->n_valid; i++) {
+        if (p_sub == NULL) {
+            local_sum += p1[i] * p2[i] * P->pixpond[i - P->n_extra];
+        } else {
+            local_sum +=
+                p1[i] * (p2[i] - p_sub[i]) * P->pixpond[i - P->n_extra];
+        }
+    }
+
+    // reduce the results for valid pixels
+    MPI_Allreduce(&local_sum, &result, 1, MPI_DOUBLE, MPI_SUM, A->comm);
+
+    // contributions from extra pixels
+    for (int i = 0; i < P->n_extra; i++) {
+        if (p_sub == NULL) {
+            result += p1[i] * p2[i];
+        } else {
+            result = p1[i] * (p2[i] - p_sub[i]);
+        }
+    }
+
+    return result;
 }
